944a28d9caa99b19cf3453b02ead429f989b5b84
Huy <lee@Huy.local>
2022-08-22 12:54:51 +0700

    #

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './styles.css'
 
-class User extends React.Component {
+class User extends Component {
     render() {
         return (
             <div className="card">
