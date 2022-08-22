aefb91fbcb1e1244416218057ec927e4ca5d425b
Huy <lee@Huy.local>
2022-08-22 12:51:50 +0700

    #

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import User from "./components/Users";
+import data from './data.json'
+import './App.css'
+class App extends Component {
+  constructor() {
+      super();
+      this.state = { data: data };
+  }
+  handleDelete = () => {
+      this.setState({ data: [] });
+  };
+  handleImport = () => {
+      this.setState({ data });
+  };
+  render() {
+      return (
+          <div className="app-container">
+              <h1>Class List</h1>
+              <button className="btn btn-delete" onClick={this.handleDelete}>
+                  Delete
+              </button>
+              <button className="btn btn-add" onClick={this.handleImport}>
+                  Import Data
+              </button>
+              <div>
+                  {this.state.data.length === 0 ? (
+                      <h2>Class list emty</h2>
+                  ) : (
+                      this.state.data.map((item, idx) => (
+                          <User
+                              key={idx}
+                              name={item.name}
+                              age={item.age}
+                              avatar={item.avatar}
+                              country={item.country}
+                          />
+                      ))
+                  )}
+              </div>
+          </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/Users/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles.css'
+
+class User extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-left">
+                    <img src={this.props.avatar} alt="avatar"/>
+                </div>
+                <div className="card-right">
+                    <h2>{this.props.name}</h2>
+                    <p><em>Age: </em>{this.props.age}</p>
+                    <p><em>Country: </em>{this.props.country}</p>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+
+export default User
